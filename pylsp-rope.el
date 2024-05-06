64fbe258c6a9637716767c19a965b890538d6e3d
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2024-05-06 16:28:07 +0200

    Initial commit

---
+++ pylsp-rope.el
@@ -0,0 +1,33 @@
+;;; pylsp-rope.el --- Pylsp rope commands -*- lexical-binding: t;
+;;
+;; Copyright (C) 2023 Tassilo Neubauer
+;;
+;; Author: Tassilo Neubauer <tassilo.neubauer@gmail.com>
+;; Maintainer: Tassilo Neubauer <tassilo.neubauer@gmail.com>
+;; Created: May 06, 2024
+;; Modified: May 06, 2024
+;; Version: 0.0.1
+;; Keywords: calendar comm convenience
+;; Homepage: https://github.com/tassilo/
+;;
+;; Package-Requires: ((emacs "27.1") (lsp-mode "8.0"))
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;;
+;;; Code:
+
+(require 'lsp-mode)
+
+(provide 'pylsp-rope)
